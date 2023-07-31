1ad53b139106ef27f566b699f7ee702125aa8874
morganPigeron <m.pigeron@hotmail.fr>
2023-07-31 21:32:50 +0200

    init

---
+++ dcsDef.lua
@@ -0,0 +1,14 @@
+---@param message string
+---@param showMessageBox boolean
+---@meta
+function env.info(message, showMessageBox) end
+
+---@param message string
+---@param showMessageBox boolean
+---@meta
+function env.warning(message, showMessageBox) end
+
+---@param message string
+---@param showMessageBox boolean
+---@meta
+function env.error(message, showMessageBox) end
---
+++ testscript1.lua
@@ -0,0 +1,4 @@
+-- show a alert box
+-- env.info("test alert", true)
+
+env.info("v1", true)
