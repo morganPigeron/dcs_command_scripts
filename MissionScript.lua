be323994133019948ca3c8b03dcfd1b1e1fdff74
morganPigeron <m.pigeron@hotmail.fr>
2023-08-04 22:28:13 +0200

    renamed

--- MissionScript.lua
+++ MissionScript.lua
@@ -54,35 +54,57 @@ local function setUnitName(unit, name)
     return unit
 end
 
-local function testSpawn(pos)
-    local newUnit = createLAV25()
-    newUnit = setUnitPosition(newUnit, pos.x, pos.y)
-    newUnit = setUnitName(newUnit, "test")
-    coalition.addGroup(country.id.USA, Group.Category.GROUND, newUnit)
+-- local function testSpawn(pos)
+--     local newUnit = createLAV25()
+--     newUnit = setUnitPosition(newUnit, pos.x, pos.y)
+--     newUnit = setUnitName(newUnit, "test")
+--     coalition.addGroup(country.id.USA, Group.Category.GROUND, newUnit)
+-- end
+-- missionCommands.addCommand("test spawn", nil, testSpawn, { x = 0, y = 0 })
+
+local function containsTextIgnoreCase(text, searchText)
+    local lowercaseText = string.lower(text)
+    local lowercaseSearchText = string.lower(searchText)
+    return string.find(lowercaseText, lowercaseSearchText, 1, true) ~= nil
 end
-missionCommands.addCommand("test spawn", nil, testSpawn, { x = 0, y = 0 })
+
+local function handleMarkChange(event)
+    local unitCountry
+    if event.coalition == coalition.side.RED then
+        unitCountry = country.id.RUSSIA
+    elseif event.coalition == coalition.side.BLUE then
+        unitCountry = country.id.USA
+    else
+        unitCountry = country.id.RUSSIA
+    end
+
+    if containsTextIgnoreCase(event.text, "LAV25") then
+        local newUnit = createLAV25() --TODO select unit from list
+        newUnit = setUnitPosition(newUnit, event.pos.x, event.pos.z)
+        newUnit = setUnitName(newUnit, "unit")
+        coalition.addGroup(unitCountry, Group.Category.GROUND, newUnit)
+    end
+end
 
 local e = {}
 function e:onEvent(event)
     local m = {}
-    m[#m + 1] = "Event ID: "
-    m[#m + 1] = event.id
-    if event.initiator then
-        m[#m + 1] = "\nInitiator : "
-        m[#m + 1] = event.initiator:getPlayerName()
-    end
-    if event.weapon then
-        m[#m + 1] = "\nWeapon : "
-        m[#m + 1] = event.weapon:getTypeName()
-    end
-    if event.target then
-        m[#m + 1] = "\nTarget : "
-        m[#m + 1] = event.target:getName()
-    end
+    -- m[#m + 1] = "Event ID: "
+    -- m[#m + 1] = event.id
+    -- if event.initiator then
+    --     m[#m + 1] = "\nInitiator : "
+    --     m[#m + 1] = event.initiator:getPlayerName()
+    -- end
+    -- if event.weapon then
+    --     m[#m + 1] = "\nWeapon : "
+    --     m[#m + 1] = event.weapon:getTypeName()
+    -- end
+    -- if event.target then
+    --     m[#m + 1] = "\nTarget : "
+    --     m[#m + 1] = event.target:getName()
+    -- end
 
-
-    --comment change on map
-    if event.id == 26 then
+    if event.id == world.event.S_EVENT_MARK_CHANGE then
         if event.text then
             m[#m + 1] = "\nText : "
             m[#m + 1] = event.text
@@ -91,9 +113,9 @@ function e:onEvent(event)
         if event.pos then
             m[#m + 1] = "\nPos : "
             m[#m + 1] = event.pos.x .. " " .. event.pos.z
-
-            testSpawn({ x = event.pos.x, y = event.pos.z })
         end
+
+        handleMarkChange(event)
     end
     trigger.action.outText(table.concat(m), 60)
 end
