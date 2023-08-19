32b4eea19b3506b358743219459ad809a0163bb4
morganPigeron <m.pigeron@hotmail.fr>
2023-08-19 18:25:04 +0200

    added tests

--- MissionScript.lua
+++ MissionScript.lua
@@ -1,6 +1,6 @@
 -- GLOBAL
 local MissionScript = {}
-local NB_UNITS_CREATED = 100
+MissionScript.NB_UNITS_CREATED = 100
 local DEBUG = false
 -- GLOBAL END
 
@@ -13,21 +13,21 @@ function MissionScript.setUnitPosition(unit, x, y)
     return unit
 end
 
-local function setUnitName(unit, name)
-    unit.units[1].unitId = NB_UNITS_CREATED
-    unit.units[1].name = name .. NB_UNITS_CREATED
-    unit.name = "group-" .. name .. NB_UNITS_CREATED
-    NB_UNITS_CREATED = NB_UNITS_CREATED + 1
+function MissionScript.setUnitName(unit, name)
+    unit.units[1].unitId = MissionScript.NB_UNITS_CREATED
+    unit.units[1].name = name .. MissionScript.NB_UNITS_CREATED
+    unit.name = "group-" .. name .. MissionScript.NB_UNITS_CREATED
+    MissionScript.NB_UNITS_CREATED = MissionScript.NB_UNITS_CREATED + 1
     return unit
 end
 
-local function containsTextIgnoreCase(text, searchText)
+function MissionScript.containsTextIgnoreCase(text, searchText)
     local lowercaseText = string.lower(text)
     local lowercaseSearchText = string.lower(searchText)
     return string.find(lowercaseText, lowercaseSearchText, 1, true) ~= nil
 end
 
-local function printTable(title, rows)
+function MissionScript.printTable(title, rows)
     local m = {}
     m[#m + 1] = "\n" .. title .. ": \n"
     for _, row in ipairs(rows) do
@@ -36,7 +36,7 @@ local function printTable(title, rows)
     trigger.action.outText(table.concat(m), 60)
 end
 
-local function getCountryIdFromCoalition(event)
+function MissionScript.getCountryIdFromCoalition(event)
     local unitCountry
     if event.coalition == coalition.side.RED then
         unitCountry = country.id.RUSSIA
@@ -47,30 +47,30 @@ local function getCountryIdFromCoalition(event)
     end
     return unitCountry
 end
+
 -- Generic Functions END
 
-
-local GROUND_UNIT = {}
+MissionScript.GROUND_UNIT = {}
 -- Function to add unitName-dcsName pairs to the table
-function GROUND_UNIT:addUnitPair(unitName, dcsName)
+function MissionScript.GROUND_UNIT:addUnitPair(unitName, dcsName)
     local name = string.upper(unitName)
     self[name] = dcsName
 end
 
 -- Function to get dcsName from unitName
-function GROUND_UNIT:getDCSName(unitName)
+function MissionScript.GROUND_UNIT:getDCSName(unitName)
     local name = string.upper(unitName)
     return self[name]
 end
 
 -- Function to check if unitName exist
-function GROUND_UNIT:isUnitExist(unitName)
+function MissionScript.GROUND_UNIT:isUnitExist(unitName)
     local name = string.upper(unitName)
     return self[name] ~= nil
 end
 
 -- Function to create unit, must check if exist before!
-function GROUND_UNIT:create(unitName)
+function MissionScript.GROUND_UNIT:create(unitName)
     local dcsName = self:getDCSName(unitName)
     return {
         ["visible"] = false,
@@ -110,16 +110,16 @@ function GROUND_UNIT:create(unitName)
 end
 
 -- Function to spawn unit at x and y position with a certain country
-function GROUND_UNIT:spawn(unitName, x, y, unitCountryId)
+function MissionScript.GROUND_UNIT:spawn(unitName, x, y, unitCountryId)
     if self:isUnitExist(unitName) then
         local newUnit = self:create(unitName)
         newUnit = MissionScript.setUnitPosition(newUnit, x, y)
-        newUnit = setUnitName(newUnit, "unit")
+        newUnit = MissionScript.setUnitName(newUnit, "unit")
         coalition.addGroup(unitCountryId, Group.Category.GROUND, newUnit)
     end
 end
 
-function GROUND_UNIT:getAllSpawnableUnits()
+function MissionScript.GROUND_UNIT:getAllSpawnableUnits()
     local units = {}
     for unitName, value in pairs(self) do
         if type(value) ~= "function" then
@@ -130,18 +130,18 @@ function GROUND_UNIT:getAllSpawnableUnits()
 end
 
 local function handleSpawnCommand(event)
-    GROUND_UNIT:spawn(event.text, event.pos.x, event.pos.z, getCountryIdFromCoalition(event))
+    MissionScript.GROUND_UNIT:spawn(event.text, event.pos.x, event.pos.z, MissionScript.getCountryIdFromCoalition(event))
 end
 
 local function handleScriptCommand(event)
-    if containsTextIgnoreCase(event.text, "DEBUG ON") then
+    if MissionScript.containsTextIgnoreCase(event.text, "DEBUG ON") then
         DEBUG = true
-    elseif containsTextIgnoreCase(event.text, "DEBUG OFF") then
+    elseif MissionScript.containsTextIgnoreCase(event.text, "DEBUG OFF") then
         DEBUG = false
-    elseif containsTextIgnoreCase(event.text, "DEBUG") then
+    elseif MissionScript.containsTextIgnoreCase(event.text, "DEBUG") then
         DEBUG = not DEBUG
-    elseif containsTextIgnoreCase(event.text, "list") or containsTextIgnoreCase(event.text, "liste") then
-        printTable("Liste", GROUND_UNIT:getAllSpawnableUnits())
+    elseif MissionScript.containsTextIgnoreCase(event.text, "list") or MissionScript.containsTextIgnoreCase(event.text, "liste") then
+        MissionScript.printTable("Liste", MissionScript.GROUND_UNIT:getAllSpawnableUnits())
     end
 end
 
@@ -152,12 +152,12 @@ end
 
 
 -- Entry point
-GROUND_UNIT:addUnitPair("LAV25", "LAV-25")
-GROUND_UNIT:addUnitPair("MORTIER", "2B11 mortar")
-GROUND_UNIT:addUnitPair("IGLA", "SA-18 Igla manpad")
-GROUND_UNIT:addUnitPair("BTR", "BTR-80")
-GROUND_UNIT:addUnitPair("TUNGUSKA", "2S6 Tunguska")
-GROUND_UNIT:addUnitPair("SHILKA", "ZSU-23-4 Shilka")
+MissionScript.GROUND_UNIT:addUnitPair("LAV25", "LAV-25")
+MissionScript.GROUND_UNIT:addUnitPair("MORTIER", "2B11 mortar")
+MissionScript.GROUND_UNIT:addUnitPair("IGLA", "SA-18 Igla manpad")
+MissionScript.GROUND_UNIT:addUnitPair("BTR", "BTR-80")
+MissionScript.GROUND_UNIT:addUnitPair("TUNGUSKA", "2S6 Tunguska")
+MissionScript.GROUND_UNIT:addUnitPair("SHILKA", "ZSU-23-4 Shilka")
 
 local eventHandler = {}
 function eventHandler:onEvent(event)
--- testSuite.lua
+++ testSuite.lua
@@ -5,12 +5,19 @@ world = {
     addEventHandler = function(eventHandler)
     end
 }
+coalition = {
+}
+Group = {
+    Category = {
+        Ground = "Ground"
+    }
+}
+
 local mis = require('MissionScript')
 
--- LuaUnit test class
-TestSetUnitPosition = {}
+TestGenericFunctions = {}
 
-function TestSetUnitPosition:testPositionSetting()
+function TestGenericFunctions:testPositionSetting()
     local unit = {
         units = {
             [1] = {
@@ -31,6 +38,152 @@ function TestSetUnitPosition:testPositionSetting()
     lu.assertEquals(result.units[1].y, newY)
     lu.assertEquals(result.x, newX)
     lu.assertEquals(result.y, newY)
+end
+
+function TestGenericFunctions:testNameSetting()
+    local unit = {
+        units = {
+            [1] = {
+                unitId = 0,
+                name = ""
+            }
+        },
+        name = ""
+    }
+
+    local testName = "TestUnit"
+
+    local result = mis.setUnitName(unit, testName)
+
+    lu.assertEquals(result.units[1].unitId, 100)
+    lu.assertEquals(result.units[1].name, testName .. 100)
+    lu.assertEquals(result.name, "group-" .. testName .. 100)
+    lu.assertEquals(mis.NB_UNITS_CREATED, 101) -- NB_UNITS_CREATED should be incremented
+end
+
+function TestGenericFunctions:testTextContainment()
+    local text = "Hello, World!"
+    local searchText = "world"
+
+    -- Test that searchText is contained in text, ignoring case
+    local result = mis.containsTextIgnoreCase(text, searchText)
+    lu.assertTrue(result)
+
+    -- Test with different cases
+    local searchText2 = "HeLLo"
+    local searchText3 = "WORLD!"
+
+    local result2 = mis.containsTextIgnoreCase(text, searchText2)
+    local result3 = mis.containsTextIgnoreCase(text, searchText3)
+
+    lu.assertTrue(result2)
+    lu.assertTrue(result3)
+end
+
+function TestGenericFunctions:testTextNonContainment()
+    local text = "Hello, World!"
+    local searchText = "foo"
+
+    -- Test that searchText is not contained in text, ignoring case
+    local result = mis.containsTextIgnoreCase(text, searchText)
+    lu.assertFalse(result)
+end
+
+TestGroundUnitTable = {}
+
+function TestGroundUnitTable:setUp()
+    -- Create a new GROUND_UNIT table for each test
+    self.groundUnit = {}
+
+    -- Load the functions from MissionScript.GROUND_UNIT
+    self.groundUnit.addUnitPair = mis.GROUND_UNIT.addUnitPair
+    self.groundUnit.getDCSName = mis.GROUND_UNIT.getDCSName
+    self.groundUnit.isUnitExist = mis.GROUND_UNIT.isUnitExist
+    self.groundUnit.create = mis.GROUND_UNIT.create
+    self.groundUnit.getAllSpawnableUnits = mis.GROUND_UNIT.getAllSpawnableUnits
+    self.groundUnit.spawn = mis.GROUND_UNIT.spawn
+
+    -- Initialize the GROUND_UNIT with sample data
+    self.groundUnit:addUnitPair("LAV25", "LAV-25")
+    self.groundUnit:addUnitPair("BTR", "BTR-80")
+    -- Add more sample data as needed
+end
+
+function TestGroundUnitTable:testAddUnitPair()
+    local unitName = "TestUnit"
+    local dcsName = "Test-DCS-Name"
+
+    self.groundUnit:addUnitPair(unitName, dcsName)
+    lu.assertEquals(self.groundUnit:getDCSName(unitName), dcsName)
+end
+
+function TestGroundUnitTable:testGetDCSName()
+    local unitName = "LAV25"
+    local expectedDCSName = "LAV-25"
+
+    local resultDCSName = self.groundUnit:getDCSName(unitName)
+    lu.assertEquals(resultDCSName, expectedDCSName)
+end
+
+function TestGroundUnitTable:testIsUnitExist()
+    local existingUnitName = "LAV25"
+    local nonExistingUnitName = "NonExistentUnit"
+
+    lu.assertTrue(self.groundUnit:isUnitExist(existingUnitName))
+    lu.assertFalse(self.groundUnit:isUnitExist(nonExistingUnitName))
+end
+
+function TestGroundUnitTable:testCreate()
+    local unitName = "LAV25"
+    local expectedDCSName = "LAV-25"
+
+    local result = self.groundUnit:create(unitName)
+
+    lu.assertEquals(result.units[1].type, expectedDCSName)
+    lu.assertFalse(result.visible)
+    lu.assertTrue(result.taskSelected)
+    lu.assertEquals(result.groupId, 2)
+    -- Add more assertions for other properties
+end
+
+function TestGroundUnitTable:testSpawn()
+    -- Mock setUnitPosition and setUnitName
+    mis.setUnitPosition = function(unit, x, y)
+        unit.x = x
+        unit.y = y
+        return unit
+    end
+    mis.setUnitName = function(unit, name)
+        unit.name = name
+        return unit
+    end
+
+    local unitName = "LAV25"
+    local x = 100
+    local y = 200
+    local unitCountryId = 1
+
+    local addGroupCalled = false
+    -- Mock only the part of coalition.addGroup related to adding a group
+    coalition.addGroup = function(countryId, category, groupData)
+        addGroupCalled = true
+    end
+
+    -- Test the spawn function when unit exists
+    self.groundUnit:spawn(unitName, x, y, unitCountryId)
+    lu.assertTrue(addGroupCalled)
+
+    -- Test the spawn function when unit does not exist
+    addGroupCalled = false
+    self.groundUnit:spawn("NonExistentUnit", x, y, unitCountryId)
+    lu.assertFalse(addGroupCalled)
+end
+
+function TestGroundUnitTable:testGetAllSpawnableUnits()
+    local units = self.groundUnit:getAllSpawnableUnits()
+
+    lu.assertEquals(#units, 2) -- Number of units added in setUp
+    -- Add more assertions for the content of units
 end
 
 -- Run the tests
